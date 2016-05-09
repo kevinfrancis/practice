5fba15978b0e1a918fb3b3bde709e008be6dd7af
Kevin Francis <kfrancis@brocade.com>
2016-05-09 11:56:06 -0700

    implement binary addition

--- bitwise/multiply.cpp
+++ bitwise/multiply.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
+
+/*
+ * Addition
+ */
+static inline uint32_t add(uint32_t x, uint32_t y)
+{
+    uint32_t result = 0;
+    int sum_bit = 0, carry_bit = 0;
+    int lsh = 0;
+    
+    for (; x | y; x >>= 1, y >>= 1) {
+        uint32_t x_bit = x & 1;
+        uint32_t y_bit = y & 1;
+        
+        sum_bit = x_bit ^ y_bit ^ carry_bit;
+        carry_bit = (((x_bit & y_bit) == 0) ? (carry_bit & (x_bit | y_bit)): 1);
+
+        result |= (sum_bit << lsh++);
+    }
+
+    result |= carry_bit << lsh;
+
+    return result;
+}
+
+
+
 /*
   Example:
      10 X 1011
@@ -19,7 +46,11 @@ uint32_t multiply(uint32_t x, uint32_t y)
     
     for (; y; y >>= 1) {
         if (y & 1) {
+#ifdef ADDITION_ALLOWED
             result += x;
+#else
+            result = add(result, x);
+#endif
         }
         x <<= 1;
     }
@@ -33,6 +64,7 @@ int main(int argc, char* argv[])
     uint32_t y = std::stoi(argv[2]);
     
     std::cout << multiply(x, y) << std::endl;
+    std::cout << "Correct ans = " << x * y << std::endl;
 
     return 0;
 }
