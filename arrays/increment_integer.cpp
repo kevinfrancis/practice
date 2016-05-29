3ae9e603b904e45cb2f5737c6a2a25f91088e770
Kevin Francis <kfrancis@brocade.com>
2016-05-28 17:19:44 -0700

    vector integer increment

---
+++ arrays/increment_integer.cpp
@@ -0,0 +1,43 @@
+/* Compile with std=c++11  */
+#include <iostream>
+#include <vector>
+
+/**
+ * Given a vector representation of an integer (arbitrary size)
+ * Increment the integer by 1.
+ * example: <2,3,4> => <2,3,5>
+ *          <9,9,9> => <1,0,0,0>
+ */
+void increment_integer(std::vector<int>& v)
+{
+    int carry = 1;
+
+    // The usual sum & carry addition operation
+    for (auto it = v.rbegin(); it != v.rend(); it++) {
+        int sum = *it + carry;
+        *it = (sum % 10);
+        carry = (sum / 10);
+    }
+
+    // All digits are 9.  Increase the vector size by 1
+    if (carry > 0) {
+        v[0] = 1;
+        v.push_back(0);
+    }
+}
+
+void disp_vector(std::vector<int>& v)
+{
+    for (auto it = v.begin(); it != v.end(); it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    std::vector<int> v = {9,9,9,9};
+    increment_integer(v);
+    disp_vector(v);
+    return 0;
+}
