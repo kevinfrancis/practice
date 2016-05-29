df9b699e0c050abd36d9b5f10512cfbdfc558ce7
Kevin Francis <kfrancis@brocade.com>
2016-05-29 10:08:16 -0700

    handle 0 result

--- arrays/multiply_integers.cpp
+++ arrays/multiply_integers.cpp
@@ -4,21 +4,29 @@
 void disp_vector(std::vector<int>& v)
 {
     std::cout << "[ ";
-    for (auto it = v.begin(); it != v.end(); it++) {
-        std::cout << *it <<  " ";
+    for (auto i : v) {
+        std::cout << i <<  " ";
     }
     std::cout << "]" << std::endl;
 }
 
+static inline bool is_zero(std::vector<int>& x)
+{
+    return (x.size() == 1 and x.back() == 0);
+}
 
 /**
  * Find product of two integers represented as vectors
  */
 std::vector<int>* product(std::vector<int>& x, std::vector<int>& y)
 {
+    if (is_zero(x) or is_zero(y)) {
+        return new std::vector<int>(1, 0);  // Vector of size 1 with value 0
+    }
+    
     std::vector<int>* v_ptr = new std::vector<int>(x.size() + y.size(), 0);
     std::vector<int>& v = *v_ptr;
-    
+
     for (auto x_it = x.rbegin(); x_it != x.rend(); x_it++) {
         int i = v.size() - 1 - (x_it - x.rbegin());
 
@@ -40,7 +48,7 @@ std::vector<int>* product(std::vector<int>& x, std::vector<int>& y)
 
 int main(int argc, char* argv[])
 {
-    std::vector<int> x = {2, 3, 4, 9};
+    std::vector<int> x = { 0 };
     std::vector<int> y = {9, 9, 8, 1, 6, 7, 8, 9, 5, 9, 9, 9};
 
     std::cout << "      x = ";
