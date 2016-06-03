c1ac20f1984f9176aa1941e0c38887ac6618c978
Kevin Francis <kevinfran@gmail.com>
2016-06-03 13:33:07 -0700

    refactor

--- dp/max_product.cpp
+++ dp/max_product.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 /**
  * Find the sub-array with the maximum product
@@ -11,24 +12,22 @@ int max_product(std::vector<int>& v)
     int max_product = INT_MIN;
 
     for (int i = 0; i < v.size(); i++) {
-        if (longer * v[i] > 0) {
-            shorter = v[i];
-            longer *= v[i];
-            max_product = std::max(max_product,
-                                   std::max(shorter, longer));
-        } else if (longer * v[i] < 0) {
-            if ((shorter * v[i]) < 0) {
-                shorter = v[i];
-            } else {
-                shorter *= v[i];
-            }
-            longer *= v[i];
-            max_product = std::max(max_product,
-                                   std::max(shorter, longer));
-        } else { // v[i] == 0
+		if (v[i] == 0) {
             shorter = longer = 1; // reset
             max_product = std::max(max_product, v[i]);
-        }
+        } else {
+			longer *= v[i];
+
+			if (longer > 0) {
+				shorter = v[i];
+			} else { // longer < 0
+				shorter *= v[i];
+				if (shorter < 0)
+					shorter = v[i];
+			}
+			max_product = std::max(max_product,
+								   std::max(shorter, longer));
+		}
     }
 
     return max_product;
@@ -45,15 +44,29 @@ void disp_vector(std::vector<int>& v)
 
 int main()
 {
-    int arr[] = {1, -2, -3, 0, 7, -8, -2};
+	for (std::string line; std::getline(std::cin, line);) {
+		std::stringstream stream(line);
+		std::vector<int> v;
+		while (not stream.eof()) {
+			int x;
+			stream >> x;
+			v.push_back(x);
+		}
+		//disp_vector(v);
+		std::cout << max_product(v) << std::endl;
+	}
+
+#if 0
+    //int arr[] = {1, -5, 6, -2, 3};
+    //int arr[] = {1, -2, -3, 0, 7, -8, -2};
     //int arr[] = {-1, -9, -45, -34};
     //int arr[] = {-1, -3, -10, 0, 60};
     //int arr[] = {6, -3, -10, 0, 2};
-    //int arr[] = {6, -3, -10, -10, 2, -1, -1};
+    int arr[] = {6, -3, -10, -10, 2, -1, -1};
     std::vector<int> v(arr, arr + (sizeof(arr)/sizeof(arr[0])));
 
     disp_vector(v);
     std::cout << "Max product = " << max_product(v) << std::endl;
-
+#endif
     return 0;
 }
