f691e4b8bb5a529a07f26e8b3159777b4de9b96b
Kevin Francis <kfrancis@brocade.com>
2016-10-08 13:30:56 -0700

    powerset

--- dp/power_set.py
+++ dp/power_set.py
@@ -4,15 +4,18 @@ import sys
 
 # Return powerset of the given set s
 def powerset(s):
-    lst = [x for x in s]
+    lst = [x for x in s] # convert to list first.  Greatly simplifies the problem
     subsets = []
-    
+
+    # @param i      index in lst
+    # @param items  to store items belonging to the current subset
     def f(i, items):
         if i == len(lst):
             subsets.append(set(items))
         else:
+            items.append(lst[i])
             f(i+1, items)        # items without lst[i]
-            items.append(lst[i]) # items with lst[i]
+            items.pop()
             f(i+1, items) 
 
     f(0, [])
